62ddae9305594a6449f97f42ca5edc7ca648d23f
Danilo Cardilli <danilocardilli@gmail.com>
2021-12-03 17:39:32 -0300

    atualizando a classe PersonController

--- src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,16 @@
 package com.digitalinnovation.personapi.controller;
 
-public  class PersonController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook() {
+        return "API Test!";
+    }
     
 }
-classe
